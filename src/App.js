cba7b57233b73728021b71380310e541669c56b1
chirag bansal <chirageng.stu22@gmail.com>
2024-04-16 22:25:33 +0530

    location  under range finder algo ready

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import './App.css';
-import { faker } from '@faker-js/faker';
-
-let data = [];
-
-for (let i = 0; i < 1000; i++) {
-  const fullName = faker.person.fullName();
-  const latitude = faker.location.latitude({ max: 30.7, min: 30.2, precision: 5 });
-  const longitude = faker.location.longitude({ max: 76.4, min: 76.3, precision: 5 });
-
-  data.push({ fullname : fullName, latitude :  latitude,  longitude : longitude });
-}
-
-console.log(JSON.stringify(data));
+import finaldata from "./data.json"
+import { useState } from 'react';
 
 function App() {
+
+  const [location, setLocation] = useState([]);
+  const R = 6371;
+
+  const finder = (range, givenlocation) => {
+
+    finaldata.forEach(element => {
+      
+      const difflat = element.latitude * (Math.PI / 180) - givenlocation.latitude * (Math.PI / 180);
+      const difflon = element.longitude * (Math.PI / 180) - givenlocation.longitude * (Math.PI / 180);
+
+     let a = Math.sin(difflat / 2) ** 2 + Math.cos(element.latitude * (Math.PI / 180)) * Math.cos(givenlocation.latitude * (Math.PI / 180)) * Math.sin(difflon / 2) ** 2
+     let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+     let distance = R * c
+
+      if(distance <= range) 
+      setLocation(prevvalue=>[element , ...prevvalue]);
+    });
+  }
+
   return (
     <div className="App">
-      
+      <button onClick={()=>{finder(10 , {
+         latitude : 40.68894,
+         longitude : 76.39308
+
+      })}}>finder</button>
+
+      {JSON.stringify(location)}
     </div>
   );
 }
