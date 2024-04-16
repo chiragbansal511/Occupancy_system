c998ae2752d90f3735101d14a03dd9af19688c0b
chirag bansal <chirageng.stu22@gmail.com>
2024-04-17 00:44:27 +0530

    range and type set

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import "@maptiler/sdk/dist/maptiler-sdk.css";
 
 function App() {
   const [location, setLocation] = useState([]);
+  const [type, setType] = useState("hospital");
+  const [select, setSelect] = useState("none");
+  const [range, setRange] = useState(0);
+  const [markers, setMarkers] = useState([]);
+
   const R = 6371;
   const mapContainer = useRef(null);
   const map = useRef(null);
@@ -14,6 +19,8 @@ function App() {
   maptilersdk.config.apiKey = 'NaYtUF3TXs9dILENQReB';
 
   const finder = (range, givenLocation) => {
+    setLocation([]);
+
     finaldata.forEach(element => {
       const difflat = element.latitude * (Math.PI / 180) - givenLocation.latitude * (Math.PI / 180);
       const difflon = element.longitude * (Math.PI / 180) - givenLocation.longitude * (Math.PI / 180);
@@ -26,11 +33,9 @@ function App() {
         setLocation(prevvalue => [element, ...prevvalue]);
       }
     });
-
   }
 
   useEffect(() => {
-    console.log("hello")
     if (!map.current) {
       map.current = new maptilersdk.Map({
         container: mapContainer.current,
@@ -38,30 +43,50 @@ function App() {
         center: [india.lng, india.lat],
         zoom: zoom
       });
+    } else {
+      markers.forEach(marker => marker.remove());
     }
 
-    else {
+    const newMarkers = [];
+    location.forEach(element => {
+      const customMarker = document.createElement('div');
+      customMarker.className = type;
 
-      if(map.current.markers != undefined) map.current.markers.forEach(marker => marker.remove());
-      if(location != undefined)
-      {
-        
-        location.forEach(element => {
-      
-          new maptilersdk.Marker({ color: "#FF0000" })
-            .setLngLat([element.longitude, element.latitude])
-            .addTo(map.current);
-        }); 
-      }
-    }
-  }, [india.lng, india.lat, zoom , location]);
+      const newMarker = new maptilersdk.Marker({
+        element: customMarker
+      })
+        .setLngLat([element.longitude, element.latitude])
+        .addTo(map.current);
+
+      newMarkers.push(newMarker);
+    });
+
+    setMarkers(newMarkers);
+  }, [india.lng, india.lat, zoom, location, type]);
 
   return (
     <div className="App">
+
+      <div onClick={() => {
+        if (select === "none")
+          setSelect("flex");
+        else
+          setSelect("none");
+      }}>
+        Select
+        <div style={{ display: select, flexDirection: 'column' }}>
+          <div onClick={() => { setType("hospital") }}>Hospitals</div>
+          <div onClick={() => { setType("hotels") }}>Hotels</div>
+          <div onClick={() => { setType("pump") }}>Pump</div>
+        </div>
+      </div>
+
+      <input type="range" min="0" max="100" defaultValue="0" onChange={(e) => setRange(e.target.value)} />
+
       <button onClick={() => {
-        finder(1, {
+        finder(range, {
           latitude: 30.68894,
-        longitude: 76.39308
+          longitude: 76.39308
         })
       }}>Find Locations</button>
 
